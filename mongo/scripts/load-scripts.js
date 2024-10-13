3d86c27928bf76338bcf1442bcd3944c23fc14bd
ForestMint <90900993+ForestMint@users.noreply.github.com>
2024-10-13 17:09:45 +0200

    _to_rebase : add mongo microservice and docker-compose.yaml

---
+++ mongo/scripts/load-scripts.js
@@ -0,0 +1,33 @@
+db = connect( 'mongodb://localhost/games' );
+db.games.insertMany( [
+   {
+      EV : '1st ACOM Cup',
+      RO : 1,
+      PB : 'Guy Tare',
+      BR : '8k' ,
+      PW : 'Jim Nastik' ,
+      WR : '12k' ,
+      KM : 5.5 ,
+      RE : "W+4.5", 
+      DT : 1993 ,
+      moves : [
+        {'side':'B', 'coordinates': 'qd'},
+        {'side':'W', 'coordinates': 'cd'}
+      ]
+   },
+   {
+    EV : '1st ACOM Cup',
+    RO : 1,
+    PB : 'Jean Bombeur',
+    BR : '15k' ,
+    PW : 'Maud Zarella' ,
+    WR : '17k' ,
+    KM : 5.5 ,
+    RE : "W+7.5", 
+    DT : 1994 ,
+    moves : [
+      {'side':'B', 'coordinates': 'df'},
+      {'side':'W', 'coordinates': 'ce'}
+    ]
+ }
+] )
