42a492773c573d6292348f33d1a3748f58f65b89
Nico <n.desdames@gmail.com>
2024-12-07 21:35:21 +0100

    _to_rebase : successfully recognizes game but fails to send back true numbers of captures because I can't find this function in package

--- GoStreamDetection/GoGamePool.py
+++ GoStreamDetection/GoGamePool.py
@@ -13,4 +13,19 @@ class GoGamePool():
     def get_captures_from_game(self, game_uuid):
 
         sente_package_game = self.games[game_uuid].game
+        print(sente_package_game)
+        print(sente_package_game)
+
+        #print(sente_package_game.get_results())
+        #print(sente_package_game.get_results())
+
+        #print(sente_package_game.score)
+
+        #print(sente_package_game.score())
+
+        print(sente_package_game.get_winner())
+
+
+
+
         return {"white":7, "black":5}
